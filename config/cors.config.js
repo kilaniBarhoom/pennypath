b9ba25d2840e94a6e3fe853a58f1ce508b1d4e52
Ibrahim Kilani <ibrahim-kelani@hotmail.com>
2024-10-20 13:38:43 +0300

    Update cors.config.js added forwarded port url

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,4 +1,4 @@
-export const allowedOrigins = [process.env.CLIENT_URL.split(" "), "http://localhost:5173"];
+export const allowedOrigins = [process.env.CLIENT_URL.split(" "), "http://localhost:5173", "https://4brrv5l4-5173.euw.devtunnels.ms/"];
 // export const allowedOrigins = ["http://localhost:5173"];
 
 
