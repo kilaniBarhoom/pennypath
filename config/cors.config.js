94f63ed518dd76b91382f37446e9fa222226b520
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-11-19 09:53:19 +0200

    last commit to check for faulty repo

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,4 +1,4 @@
-export const allowedOrigins = [process.env.CLIENT_URL.split(" "), "http://localhost:5173"];
+export const allowedOrigins = [process.env.CLIENT_URL.split(" ")];
 
 
 const corsOptions = {
