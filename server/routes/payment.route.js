dddefbc76b29cf6bf2b2a6a8db3ad520a4ffde75
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2025-01-21 19:05:09 +0200

    feat(auth): simplify payment route authorization by removing role checks for create and get single payment

--- server/routes/payment.route.js
+++ server/routes/payment.route.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { OK } from '../constants/status.constants.js';
 import * as controller from '../controllers/payment.controller.js';
-import { auth, hasRole } from '../middleware/auth.middleware.js';
+import { auth } from '../middleware/auth.middleware.js';
 import catcher from '../middleware/catcher.middleware.js';
-import Roles from "../utils/authRoles.js";
 
 const router = express.Router()
 
@@ -15,13 +14,13 @@ router.use("/health", (req, res) => {
 router.route('/')
     .all(auth)
     .get(catcher(controller.getAllPayments))
-    .post(hasRole(Roles.ADMIN, Roles.SUPERADMIN), catcher(controller.createPayment))
+    .post(catcher(controller.createPayment))
 
 // router.get('/analytics', auth, hasRole(Roles.ADMIN, Roles.SUPERADMIN), catcher(controller.getAnalytics))
-router.get('/:paymentId', auth, hasRole(Roles.ADMIN, Roles.SUPERADMIN, Roles.USER), catcher(controller.getSinglePayment))
 
 router.route('/:paymentId')
-    .all(auth, hasRole(Roles.ADMIN, Roles.SUPERADMIN))
+    .all(auth)
+    .get(catcher(controller.getSinglePayment))
     .delete(catcher(controller.deletePayment))
     .put(catcher(controller.editPayment))
 
