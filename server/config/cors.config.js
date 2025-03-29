4bbbff43de61efec926f560a3557c070827b2810
Ibrahim Kilani <ibrahim-kelani@hotmail.com>
2025-03-30 00:10:52 +0200

    Update cors.config.js

--- server/config/cors.config.js
+++ server/config/cors.config.js
@@ -1,6 +1,6 @@
+ export const allowedOrigins = [process.env.CLIENT_URL.split(" ")];
 // export const allowedOrigins = [process.env.CLIENT_URL.split(" ")];
-// export const allowedOrigins = [process.env.CLIENT_URL.split(" ")];
-export const allowedOrigins = ["http://localhost:5173", "http://192.168.1.8:5173", "https://j7v38ztk-5173.euw.devtunnels.ms"];
+// export const allowedOrigins = ["http://localhost:5173", "http://192.168.1.8:5173", "https://j7v38ztk-5173.euw.devtunnels.ms"];
 
 
 const corsOptions = {
